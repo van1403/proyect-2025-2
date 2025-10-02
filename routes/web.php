018fced87db58f5ff545dad6bd2578e1c7599fbc
van1403 <leydisabino5@gmail.com>
2025-10-02 10:40:52 -0500

    Archivo vista de bienvenido

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludo', function () {
+    return 'Hola a todos';
+});
+
+Route::get('/bienvenidos', function () {
+    return view('bienvenidos');
+});
